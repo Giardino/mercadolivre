389640e87d78a7a7f5bae697970586b615ad0f17
Luciano Giardino <luciano.giardino@gmail.com>
2024-08-15 22:09:32 -0300

    feat: created SearchProductActivity

--- app/src/main/java/com/lucianogiardino/mercadolivre/presentation/searchproducts/SearchProductsActivity.kt
+++ app/src/main/java/com/lucianogiardino/mercadolivre/presentation/searchproducts/SearchProductsActivity.kt
@@ -1,4 +1,4 @@
-package com.lucianogiardino.mercadolivre
+package com.lucianogiardino.mercadolivre.presentation.searchproducts
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.lucianogiardino.mercadolivre.ui.theme.MercadolivreTheme
+import com.lucianogiardino.mercadolivre.presentation.theme.MercadolivreTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
